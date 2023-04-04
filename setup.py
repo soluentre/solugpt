d3447f5259fe0c423464cdf6cde8605f2ed6ee28
Shiyao Wang <45439609+soluentre@users.noreply.github.com>
2023-04-04 17:29:06 -0400

    update setup 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unicellgpt',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
